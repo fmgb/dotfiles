38aa6d5e996f15628d5d368b39d27c8bf2602fa8
fmgb <fmgb92@gmail.com>
2015-08-16 20:27:41 +0200

    Modify .gitignore for the temporally files and add the emacs' config

---
+++ .emacs.d/custom.el
@@ -0,0 +1,78 @@
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   (quote
+    ("756597b162f1be60a12dbd52bab71d40d6a2845a3e3c2584c6573ee9c332a66e" default)))
+ '(doc-view-continuous t))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(calendar-christian-all-holidays-flag t)
+ '(calendar-date-style (quote european))
+ '(calendar-holidays (quote ((holiday-fixed 1 1 "Año nuevo") (holiday-fixed 2 14 "San Valentin") (holiday-fixed 8 15 "Día de la Madre") (holiday-float 6 0 3 "Día del Padre") (holiday-fixed 9 15 "Día de la Independencia") (holiday-fixed 5 1 "Día Internacional del Trabajo") (holiday-fixed 7 25 "Anexión del Partido de Nicoya a Costa Rica") (holiday-fixed 8 2 "Día de la Virgen de los Ángeles") (holiday-fixed 9 12 "Día de las Culturas") (holiday-fixed 10 31 "Víspera de Todos los Santos") (holiday-fixed 11 19 "Día Internacional del Hombre") (holiday-easter-etc) (holiday-fixed 12 25 "Navidad") (if calendar-christian-all-holidays-flag (append (holiday-fixed 1 6 "Epifanía") (holiday-julian 12 25 "Navidad (calendario Juleano)") (holiday-greek-orthodox-easter) (holiday-fixed 8 15 "Asunción de María") (holiday-advent 0 "Adviento"))) (solar-equinoxes-solstices))))
+ '(company-scrollbar-bg ((t (:background "#191919"))))
+ '(company-scrollbar-fg ((t (:background "#0c0c0c"))))
+ '(company-tooltip ((t (:inherit default :background "#050505"))))
+ '(company-tooltip-common ((t (:inherit font-lock-constant-face))))
+ '(company-tooltip-selection ((t (:inherit font-lock-function-name-face))))
+ '(custom-safe-themes (quote ("c5a044ba03d43a725bd79700087dea813abcb6beb6be08c7eb3303ed90782482" "9dae95cdbed1505d45322ef8b5aa90ccb6cb59e0ff26fef0b8f411dfc416c552" "3a727bdc09a7a141e58925258b6e873c65ccf393b2240c51553098ca93957723" "6a37be365d1d95fad2f4d185e51928c789ef7a4ccf17e7ca13ad63a8bf5b922f" default)))
+ '(elpy-interactive-python-command "ipython")
+ '(elpy-mode-hook (quote (hl-line-mode)))
+ '(elpy-modules (quote (elpy-module-company elpy-module-eldoc elpy-module-pyvenv elpy-module-highlight-indentation elpy-module-yasnippet)))
+ '(elpy-rpc-backend "jedi")
+ '(elpy-rpc-python-command "python")
+ '(flycheck-check-syntax-automatically (quote (save idle-change new-line mode-enabled)))
+ '(flycheck-disabled-checkers (quote (emacs-lisp-checkdoc)))
+ '(flycheck-idle-change-delay 2.0)
+ '(flycheck-indication-mode (quote right-fringe))
+ '(global-flycheck-mode t nil (flycheck))
+ '(holiday-christian-holidays (quote ((holiday-easter-etc) (holiday-fixed 12 25 "Navidad") (if calendar-christian-all-holidays-flag (append (holiday-fixed 1 6 "Epifanía") (holiday-julian 12 25 "Navidad (calendario Juleano)") (holiday-greek-orthodox-easter) (holiday-fixed 8 15 "Asunción de María") (holiday-advent 0 "Adviento"))))))
+ '(holiday-general-holidays (quote ((holiday-fixed 1 1 "Año nuevo") (holiday-fixed 2 14 "San Valentin") (holiday-fixed 8 15 "Día de la Madre") (holiday-float 6 0 3 "Día del Padre") (holiday-fixed 9 15 "Día de la Independencia") (holiday-fixed 5 1 "Día Internacional del Trabajo") (holiday-fixed 7 25 "Anexión del Partido de Nicoya a Costa Rica") (holiday-fixed 8 2 "Día de la Virgen de los Ángeles") (holiday-fixed 9 12 "Día de las Culturas") (holiday-fixed 10 31 "Víspera de Todos los Santos"))))
+ '(holiday-other-holidays (quote ((holiday-fixed 11 19 "Día Internacional del Hombre"))))
+ '(jabber-alert-info-message-hooks (quote (jabber-info-libnotify)))
+ '(jabber-alert-message-hooks (quote (jabber-message-libnotify jabber-message-scroll)))
+ '(jabber-alert-muc-hooks (quote (jabber-muc-libnotify-personal jabber-muc-scroll)))
+ '(jabber-alert-presence-hooks (quote (jabber-presence-libnotify)))
+ '(jabber-auto-reconnect t)
+ '(jabber-chat-buffer-format "*-chat-%n-*")
+ '(jabber-chat-delayed-time-format "%d-%m-%Y %H:%M")
+ '(jabber-chat-foreign-prompt-format "[%t] %n > ")
+ '(jabber-chat-text-foreign ((t (:foreground "white smoke"))))
+ '(jabber-groupchat-buffer-format "*-gc-%n-*")
+ '(jabber-groupchat-prompt-format "[%t] %n > ")
+ '(jabber-info-message-alist (quote ((roster . "Actualizada la vizualización del roster") (browse . "Solicitud de navegación completada"))))
+ '(jabber-libnotify-app "Jabber.el")
+ '(jabber-libnotify-icon "emacs")
+ '(jabber-libnotify-message-header "Mensaje en Jabber")
+ '(jabber-libnotify-timeout 10000)
+ '(jabber-libnotify-urgency "normal")
+ '(jabber-muc-completion-delimiter ", ")
+ '(jabber-muc-private-buffer-format "*-muc-priv-%g-%n-*")
+ '(jabber-roster-buffer "*-roster-*")
+ '(org-babel-load-languages (quote ((ledger . t) (python . t) (emacs-lisp . t))))
+ '(org-modules (quote (org-bbdb org-bibtex org-crypt org-docview org-gnus org-habit org-id org-info org-irc org-mhe org-rmail org-w3m org-expiry org-invoice)))
+ '(py-autopep8-options (quote ("--ignore E711,E712,E24" "--aggressive --aggressive --aggressive")))
+ '(python-shell-interpreter "python2")
+ '(pyvenv-virtualenvwrapper-python "/usr/bin/python2")
+ '(rainbow-delimiters-depth-1-face ((t (:foreground "blue"))))
+ '(rainbow-delimiters-depth-2-face ((t (:foreground "yellow"))))
+ '(rainbow-delimiters-depth-3-face ((t (:foreground "deep pink"))))
+ '(rainbow-delimiters-depth-4-face ((t (:foreground "green"))))
+ '(rainbow-delimiters-depth-5-face ((t (:foreground "purple"))))
+ '(rainbow-delimiters-depth-6-face ((t (:foreground "orange"))))
+ '(rainbow-delimiters-depth-7-face ((t (:foreground "burlywood4"))))
+ '(rainbow-delimiters-depth-8-face ((t (:foreground "#04FFFF"))))
+ '(rainbow-delimiters-depth-9-face ((t (:foreground "white"))))
+ '(rainbow-delimiters-unmatched-face ((t (:foreground "red"))))
+ '(safe-local-variable-values (quote ((ispell-local-dictionary . english) (ispell-local-dictionary . en) (ispell-dictionary . en) (encoding . utf-8))))
+ '(show-paren-match ((((class color) (background light)) (:background "azure2"))))
+ '(tramp-default-method "ssh")
+ '(tramp-encoding-shell "/bin/bash")
+ '(vr/default-replace-preview t))
+
---
+++ .emacs.d/init.el
@@ -0,0 +1,7 @@
+(defvar t-start (current-time))
+(package-initialize)
+(require 'ob-tangle)
+(org-babel-load-file
+ (expand-file-name "configuration.org" user-emacs-directory))
+(message "Emacs ready!")
+(message "Total elapsed: %s "(float-time (time-subtract (current-time) t-start)))
